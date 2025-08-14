4c6383080af083578cffca81e5ac0042cdc4bd5d
GeorgeBinDragon <georgebindragon@gmail.com>
2025-08-14 18:00:12 +0800

    add slint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
+// slint 相关
+
+// 在 Windows 发行版本中，除了 Slint 窗口外，还需阻止控制台窗口。其他平台忽略此设置。
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+// 将 .slint 编译后的 Rust 模块引入到代码里
+slint::include_modules!();
+
+fn main() -> Result<(), slint::PlatformError> {
     println!("Hello, world!");
+
+    let main_window = MainWindow::new()?;
+    main_window.run()
 }
