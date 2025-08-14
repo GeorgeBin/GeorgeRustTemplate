9d2e30e57e6fd2caf66b34b7b32ed85d81d74be9
GeorgeBinDragon <georgebindragon@gmail.com>
2025-08-14 18:10:45 +0800

    给 exe 打包增加图标

--- build.rs
+++ build.rs
@@ -3,11 +3,11 @@ extern crate winresource;
 
 fn main() {
     slint_build::compile("ui/main.slint").expect("Slint build failed");
-    
-    // // 给 exe 添加图标
-    // if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
-    //     let mut res = winresource::WindowsResource::new();
-    //     res.set_icon("ui/static/logo/windows.ico");
-    //     res.compile().unwrap();
-    // }
+
+    // 给 exe 添加图标
+    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
+        let mut res = winresource::WindowsResource::new();
+        res.set_icon("ui/static/logo/windows.ico");
+        res.compile().unwrap();
+    }
 }
