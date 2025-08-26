6cd08f49a2a860e2dd93c3e0e04d37681da2648c
GeorgeBinDragon <georgebindragon@gmail.com>
2025-08-26 16:38:29 +0800

    资源文件放到 ui 下的 res 目录

--- build.rs
+++ build.rs
@@ -7,7 +7,7 @@ fn main() {
     // 给 exe 添加图标
     if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
         let mut res = winresource::WindowsResource::new();
-        res.set_icon("ui/static/logo/windows.ico");
+        res.set_icon("ui/res/logo/windows.ico");
         res.compile().unwrap();
     }
 }
