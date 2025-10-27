59cb54b313742ac56abd3ae0602da4a958e94807
GeorgeBinDragon <georgebindragon@gmail.com>
2025-10-27 15:07:10 +0800

    增加 ntp 示例，增加调用代码

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -3,6 +3,8 @@
 // 在 Windows 发行版本中，除了 Slint 窗口外，还需阻止控制台窗口。其他平台忽略此设置。
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::time::SystemTime;
+
 mod demo;
 mod protos;
 mod ui;
@@ -41,19 +43,34 @@ fn main() -> Result<(), slint::PlatformError> {
     //     i2 + 10
     // });
 
+    // corelib 内方法的调用
     {
-        // 工程内 cargo create 引用：core
-        core::add(1, 2);
-        let valid = core::utils::is_valid_ipv4("192.168.1.1".to_string());
-        println!("is valid ipv4 {valid}");
+        corelib::add(1, 2);
+        let valid = corelib::utils::is_valid_ipv4("192.168.1.1".to_string());
+        println!("is valid ipv4={valid}");
 
-        demo::impl_logic_for_backend(window.as_weak().clone());
+        get_ntp_time();
     }
+
+    demo::impl_logic_for_backend(window.as_weak().clone());
 
     ui::view::res(); // 引用方式一：在目录同层，创建同名 rs 文件，声明 mod
     protos::test::test_protos(); // 引用方式二：在目录内，创建 mod.rs 文件，声明 mod
 
-
+    get_ntp_time();
 
     window.run()
+}
+
+/// 工具函数：格式化 SystemTime
+pub fn format_system_time(t: SystemTime) -> String {
+    let datetime: chrono::DateTime<chrono::Local> = t.into();
+    datetime.format("%Y-%m-%d %H:%M:%S").to_string()
+}
+
+fn get_ntp_time() {
+    let client = corelib::ntp::NtpClient::new("ntp2.aliyun.com:123");
+    let result = client.sync_time();
+    let time = format_system_time(result.unwrap());
+    println!("ntp 请求结果：{:?}", time);
 }
