f552da022e774318e93a079b2fd5b68a009754a8
GeorgeBinDragon <georgebindragon@gmail.com>
2025-10-27 11:30:15 +0800

    增加 uniffi-rs 库的使用

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+uniffi::include_scaffolding!("shared");
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn is_valid_ipv4(ip: String) -> bool {
+    core::utils::is_valid_ipv4(ip)
 }
