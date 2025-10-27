add465e6f6a4af2821e5ba0b2ca953c525c77725
GeorgeBinDragon <georgebindragon@gmail.com>
2025-10-27 11:30:30 +0800

    移除无用代码

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,18 +1,5 @@
 pub mod utils;
 
-use utils::*;
-
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
