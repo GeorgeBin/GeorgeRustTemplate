15b1b3656ed3eca28f50a070544c7902d3933398
GeorgeBinDragon <georgebindragon@gmail.com>
2026-01-10 19:42:50 +0800

    uniffi：修改使用方式

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,5 +1,6 @@
-uniffi::include_scaffolding!("shared");
+uniffi::setup_scaffolding!("shared");
 
+#[uniffi::export]
 pub fn is_valid_ipv4(ip: String) -> bool {
     corelib::utils::is_valid_ipv4(ip)
 }
