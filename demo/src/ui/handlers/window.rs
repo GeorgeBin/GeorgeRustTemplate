522f0ce535ac5acd7c0d7ef4b5e1af562fba62dc
GeorgeBinDragon <georgebindragon@gmail.com>
2026-04-11 19:35:14 +0800

    warning

--- demo/src/ui/handlers/window.rs
+++ demo/src/ui/handlers/window.rs
@@ -8,45 +8,37 @@ pub fn setup(app: &AppWindow, app_handle: slint::Weak<AppWindow>) {
 
     let ah = app_handle.clone();
     app.on_drag_window(move || {
-        if custom_window_chrome {
-            if let Some(_app) = ah.upgrade() {
+        if custom_window_chrome && let Some(_app) = ah.upgrade() {
             #[cfg(not(target_os = "macos"))]
             {
                 let _ = _app.window().with_winit_window(|window| {
                     let _ = window.drag_window();
                 });
             }
-            }
         }
     });
 
     let ah = app_handle.clone();
     app.on_toggle_maximize_window(move || {
-        if custom_window_chrome {
-            if let Some(app) = ah.upgrade() {
+        if custom_window_chrome && let Some(app) = ah.upgrade() {
             let next = !app.get_window_maximized();
             app.set_window_maximized(next);
             app.window().set_maximized(next);
-            }
         }
     });
 
     let ah = app_handle.clone();
     app.on_minimize_window(move || {
-        if custom_window_chrome {
-            if let Some(app) = ah.upgrade() {
+        if custom_window_chrome && let Some(app) = ah.upgrade() {
             app.window().set_minimized(true);
-            }
         }
     });
 
     let ah = app_handle.clone();
     app.on_close_window(move || {
-        if custom_window_chrome {
-            if let Some(app) = ah.upgrade() {
+        if custom_window_chrome && let Some(app) = ah.upgrade() {
             let _ = app.hide();
             slint::quit_event_loop().ok();
-            }
         }
     });
 
